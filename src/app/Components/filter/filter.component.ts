13d15b6a92e2b4ca397c73d59749af3ca07c8bbc
mohammedsameer <mohammed.sameer@nagarro.com>
2022-05-03 15:26:22 +0530

    fixed product cards and filter value extract

--- src/app/Components/filter/filter.component.ts
+++ src/app/Components/filter/filter.component.ts
@@ -1,5 +1,4 @@
 import { Component, OnInit } from '@angular/core';
-import {MatCheckboxChange} from '@angular/material/checkbox';
 import { UserService } from 'src/app/services/user.service';
 
 @Component({
@@ -15,17 +14,37 @@ export class FilterComponent implements OnInit {
   
   public formData = {
     priceVal:"700",
-    brands:[]
+    brands: [""],
+    selected: [""]
   }
   public check = []
   results:any=[]
-  clickHandler=()=>{}
+  clickHandler=(e:any)=>{
+    
+    if(this.formData.selected.includes(e.source.value))
+      this.formData.selected=this.formData.selected.filter(i=>i!==e.source.value);
+    else
+      this.formData.selected.push(e.source.value);
+    console.log(this.formData.selected)
+  }
   ngOnInit(): void {
     
+    this.userService.getProducts().subscribe(
+      res=> this.setResult(res));
+
     
   }
-  
    
+    setResult(r:any){
+      this.results=r;
+      this.check=this.results.map((x:any)=>{
+        return x.brand;
+      })
+      for(let i=0;i<this.results.length;i++){
+        
+      }
+      this.check=[...new Set(this.check)]
+    }
     
   }
 
--- src/app/Components/product-card/product-card.component.ts
+++ src/app/Components/product-card/product-card.component.ts
@@ -1,7 +1,5 @@
 import { Component, OnInit, Input } from '@angular/core';
-
-
-
+import { UserService } from 'src/app/services/user.service';
 
 @Component({
   selector: 'app-product-card',
@@ -11,14 +9,23 @@ import { Component, OnInit, Input } from '@angular/core';
 export class ProductCardComponent implements OnInit {
   
  
-  constructor() {
-    
-   }
-   
-
-  ngOnInit(): void {
-
-  }
-  
-
+  constructor(public userService:UserService) {}
+    public brands = []
+    results:any=[]
+    ngOnInit(): void {
+      this.userService.getProducts().subscribe(
+        res => this.setResult(res));
+        console.log(this.results[0]);  
+      }
+      
+      setResult(r:any){
+        this.results=r;
+        this.brands=this.results.map((x:any)=>{
+          return x.brand;
+        })
+        for(let i=0;i<this.results.length;i++){
+          console.log(this.results[i].imgUrl);
+        }
+        this.brands=[...new Set(this.brands)]
+      }
 }
